75988db44ef2d391702a32867eb43c5a4ef13a3a
baddesinger5 <baddesinger5@gmail.com>
2021-01-12 13:31:57 +0300

    add removiable task (for each task)

--- main.js
+++ main.js
@@ -6,31 +6,45 @@ let getUl = document.querySelector('.list');
 addBtn.addEventListener('click', function() {
     let createLi = document.createElement('li');
     createLi.classList.add('row');
+    
 
     let createSpan = document.createElement('span');
-    createSpan.innerHTML = '&#10006;';
+
+    let createLink = document.createElement('a');
+    createLink.innerHTML = '&#10006;';
 
     getUl.appendChild(createLi);
     createLi.append(input.value);
     createLi.append(createSpan);
+    createSpan.append(createLink);
 
+   
     // //delete one task
-    // let spanDetected = document.querySelector('span');
-    // spanDetected.addEventListener('click', function(e) {
-    //     e.preventDefault();
-    //     spanDetected.remove();
-    //     createLi.remove();
-
-    // })
+    function removeItem() {
+        let eachItem = this;
+            createLink.addEventListener('click', function(e) {
+                e.preventDefault();
+                createLi.remove(eachItem);
+                createLink.remove(eachItem);
+        
+    })
+    }
+    removeItem();
+    
 
     // delete all
     const removeAll = document.querySelector('.remove-all');
     removeAll.addEventListener('click', function() {
         createLi.remove();
         createSpan.remove();
+        localStorage.clear();
     })
 
 })
+
+
+
+
 
 
 
