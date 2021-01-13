de83264f7a19bd838bfa0d35a773c6ee8d3fb02b
baddesinger5 <baddesinger5@gmail.com>
2021-01-13 13:56:21 +0300

    add all functions

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
 //create task
+// const addBtn = document.querySelector('.add');
+// let input = document.querySelector("#input[type='text']");
+// let getUl = document.querySelector('.list');
+// let emptyArray = [];
+
+// let liMaker = addBtn.addEventListener('click', function() {
+//     let createLi = document.createElement('li');
+//     createLi.classList.add('row');
+
+//     let createSpan = document.createElement('span');
+
+//     let createLink = document.createElement('a');
+//     createLink.innerHTML = '&#10006;';
+
+//     getUl.appendChild(createLi);
+//     createLi.append(input.value);
+//     createLi.append(createSpan);
+//     createSpan.append(createLink);
+
+//     // //delete one task
+//     function removeItem() {
+//         let eachItem = this;
+//             createLink.addEventListener('click', function(e) {
+//                 e.preventDefault();
+//                 createLi.remove(eachItem);
+//                 createLink.remove(eachItem);
+//     })
+//     } removeItem();
+    
+//     // delete all
+//     const removeAll = document.querySelector('.remove-all');
+//     removeAll.addEventListener('click', function() {
+//         createLi.remove();
+//         createSpan.remove();
+//         localStorage.clear();
+//     })
+// })
+
+//===========================================================================
+
+//create task
 const addBtn = document.querySelector('.add');
 let input = document.querySelector("#input[type='text']");
 let getUl = document.querySelector('.list');
 
-addBtn.addEventListener('click', function() {
+//create li
+const makeLi = function (value) {
+    
     let createLi = document.createElement('li');
     createLi.classList.add('row');
-    
 
     let createSpan = document.createElement('span');
 
     let createLink = document.createElement('a');
     createLink.innerHTML = '&#10006;';
 
-    getUl.appendChild(createLi);
-    createLi.append(input.value);
+    createLi.append(value);
     createLi.append(createSpan);
     createSpan.append(createLink);
-
-   
+    
+        
     // //delete one task
     function removeItem() {
         let eachItem = this;
@@ -26,22 +67,42 @@ addBtn.addEventListener('click', function() {
                 e.preventDefault();
                 createLi.remove(eachItem);
                 createLink.remove(eachItem);
-        
     })
-    }
-    removeItem();
-    
+    } removeItem();
 
-    // delete all
-    const removeAll = document.querySelector('.remove-all');
-    removeAll.addEventListener('click', function() {
-        createLi.remove();
-        createSpan.remove();
-        localStorage.clear();
-    })
+    return createLi;
+}
 
+/*
+	Init first list
+*/
+JSON.parse(localStorage.storageItems || '[]').map(function(n) {
+	getUl.appendChild(makeLi(n));
 })
 
+
+// delete all
+const removeAll = document.querySelector('.remove-all');
+removeAll.addEventListener('click', function() {
+/*   createLi.remove();
+  createSpan.remove(); */
+  localStorage.clear();
+  getUl.innerHTML = '';
+})
+
+
+let liMaker = addBtn.addEventListener('click', function() {
+    
+		const value = input.value;
+    
+    // Add item to storage
+    const storageItems = JSON.parse(localStorage.storageItems || '[]');
+    storageItems.push(value);
+    localStorage.storageItems = JSON.stringify(storageItems);
+    input.value = '';
+
+    getUl.appendChild(makeLi(value));
+})
 
 
 
